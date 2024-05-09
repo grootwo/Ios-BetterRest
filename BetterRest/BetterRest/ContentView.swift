b39054a88c92cd9546674dd927a9d9b91721084d
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-09 16:50:51 +0900

    Create Date with different format

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -11,14 +11,21 @@ struct ContentView: View {
     @State private var wakeUpTime = Date.now
     var body: some View {
         VStack {
-            DatePicker("Wake up time", selection: $wakeUpTime, in: Date.now..., displayedComponents: .hourAndMinute)
-                .labelsHidden()
+            Text(Date.now, format: .dateTime.year().month().day().hour().minute())
+            Text(Date.now.formatted(date: .complete, time: .standard))
         }
         .padding()
     }
     func dataExamples() {
-        let tomorrow = Date.now.addingTimeInterval(86400)
-        let range = Date.now...tomorrow
+//        var components = DateComponents()
+//        components.hour = 8
+//        components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+        
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        let hour = components.hour ?? 0
+        let minute = components.minute ?? 0
+        
     }
 }
 
